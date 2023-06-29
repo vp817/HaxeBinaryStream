2ea6d85c86c066be1db2b9df319ac328c3f75e19
vp817 <118466306+vp817@users.noreply.github.com>
2023-06-29 23:05:54 +0300

    rewrite-2

--- vp817/BinaryStream.hx
+++ vp817/BinaryStream.hx
@@ -4,42 +4,103 @@ import haxe.io.FPHelper;
 import haxe.Int32;
 import haxe.Int64;
 import haxe.io.Error;
-import haxe.io.BytesBuffer;
 import haxe.io.Bytes;
 
 var EndOfStream:Error = Error.Overflow;
-var VarInTooBig:Error = Error.Overflow;
+var VarIntTooBig:Error = Error.Overflow;
 
 class BinaryStream {
-	public var buffer:BytesBuffer;
+	public var buffer:Bytes;
+	public var writingPos:Int;
 	public var readingPos:Int;
 	public var isBigEndian:Bool;
 
-	public function new(buffer:BytesBuffer, readingPos:Int, bigEndain:Bool):Void {
+	static var ByteShiftMapTable:Map<Int, Map<Int, Array<Int>>> = [
+		16 => [0 => [8, 0], 1 => [0, 8]],
+		24 => [0 => [16, 8, 0], 1 => [0, 8, 16]],
+		32 => [0 => [24, 16, 8, 0], 1 => [0, 8, 16, 24]]
+	];
+
+	static var LimitsTable:Map<Int, Map<Int, Array<Int>>> = [
+		8 => [0 => [0x00, 0xff], 1 => [-(0x80), 0x7f]],
+		16 => [0 => [0x00, 0xffff], 1 => [-(0x8000), 0x7fff]],
+		24 => [0 => [0x00, 0x00ffffff], 1 => [-(0x800000), 0x7fffff]],
+		32 => [0 => [0x00, 0xffffffff], 1 => [-(0x80000000), 0x7fffffff]]
+	];
+
+	static public function signInt(bitSize:Int, value:Int) {
+		if (bitSize == 8) {
+			return (value + (1 << 7)) % (1 << 8) - (1 << 7);
+		}
+		return value < (1 << (bitSize - 1)) ? value : value - (1 << bitSize);
+	}
+
+	static public function limit(value:Int, bitSize:Int, signed:Bool):Int {
+		var retValue = value;
+		var limitTable:Map<Int, Array<Int>> = BinaryStream.LimitsTable[bitSize];
+		if (!signed) {
+			if (retValue < limitTable[0][0]) {
+				retValue = limitTable[0][0];
+			} else if (retValue > limitTable[0][1]) {
+				retValue &= limitTable[0][1];
+			}
+		} else {
+			retValue = BinaryStream.signInt(bitSize, retValue);
+			if (retValue < limitTable[1][0]) {
+				retValue = limitTable[1][0];
+			} else if (retValue > limitTable[1][1]) {
+				retValue &= limitTable[1][1];
+			}
+		}
+		return retValue;
+	}
+
+	static public function writeValueIntoBytes(bytes:Bytes, bitSize:Int, bigEndian:Bool, value:Int):Void {
+		var shiftMap:Map<Int, Array<Int>> = BinaryStream.ByteShiftMapTable[bitSize];
+		var byteArray:Array<Int> = bigEndian ? shiftMap[0] : shiftMap[1];
+		var i:Int = 0;
+		for (v in byteArray) {
+			bytes.set(i, value >> v);
+			++i;
+		}
+	}
+
+	static public function readValueFromBytes(bytes:Bytes, bitSize:Int, bigEndian:Bool):Int {
+		var shiftMap:Map<Int, Array<Int>> = BinaryStream.ByteShiftMapTable[bitSize];
+		var byteArray:Array<Int> = bigEndian ? shiftMap[0] : shiftMap[1];
+		var value:Int = 0;
+		var i:Int = 0;
+		for (v in byteArray) {
+			value |= bytes.get(i) << v;
+			++i;
+		}
+		return value;
+	}
+
+	public function new(buffer:Bytes, writingPos:Int, readingPos:Int, bigEndain:Bool):Void {
 		this.buffer = buffer;
+		this.writingPos = writingPos;
 		this.readingPos = readingPos;
 		this.isBigEndian = bigEndain;
 	}
 
-	static public function allocate(bigEndain:Bool):BinaryStream {
-		return new BinaryStream(new BytesBuffer(), 0, bigEndain);
-	}
-
-	public function getBytes():Bytes {
-		return this.buffer.getBytes();
+	static public function allocate(size:Int, bigEndain:Bool):BinaryStream {
+		return new BinaryStream(Bytes.alloc(size), 0, 0, bigEndain);
 	}
 
 	public function write(value:Bytes):Void {
-		if (value.length < 0 || this.buffer.length < 0 || this.eos()) {
+		if (this.eos()) {
 			throw EndOfStream;
 		}
 
-		this.buffer.add(value);
+		var bytesSize:Int = value.length;
+		this.buffer.blit(this.writingPos, value, 0, bytesSize);
+		this.writingPos += bytesSize;
 	}
 
-	public function read(offset:Int):Int {
-		this.readingPos += offset;
-		return this.getBytes().get(this.readingPos - offset);
+	public function read(size:Int):Bytes {
+		this.readingPos += size;
+		return this.buffer.sub(this.readingPos - size, this.readingPos);
 	}
 
 	public function eos():Bool {
@@ -47,6 +108,13 @@ class BinaryStream {
 	}
 
 	public function rewind():Void {
+		this.writingPos = 0;
+		this.readingPos = 0;
+	}
+
+	public function reset():Void {
+		this.buffer = Bytes.alloc(0);
+		this.writingPos = 0;
 		this.readingPos = 0;
 	}
 
@@ -56,7 +124,8 @@ class BinaryStream {
 
 	public function writeInt8(value:Int, signed:Bool):Void {
 		var temp:Bytes = Bytes.alloc(1);
-		temp.set(0, value & (signed == true ? 0x7f : 0xff));
+		value = BinaryStream.limit(value, 8, signed);
+		temp.set(0, value);
 		this.write(temp);
 	}
 
@@ -66,46 +135,31 @@ class BinaryStream {
 
 	public function writeInt16(value:Int, signed:Bool):Void {
 		var temp:Bytes = Bytes.alloc(2);
-		var v:Int = signed == true ? 0x7fff : 0xffff;
-		if (this.isBigEndian) {
-			temp.set(0, (value & v) >> 8);
-			temp.set(1, value & v);
-		} else {
-			temp.set(0, value & v);
-			temp.set(1, (value & v) >> 8);
-		}
+		value = BinaryStream.limit(value, 16, signed);
+		BinaryStream.writeValueIntoBytes(temp, 16, this.isBigEndian, value);
 		this.write(temp);
 	}
 
-	public function writeInt32(value:Int32):Void {
+	public function writeInt32(value:Int32, signed:Bool):Void {
 		var temp:Bytes = Bytes.alloc(4);
-		if (this.isBigEndian) {
-			temp.set(0, value >> 24);
-			temp.set(1, value >> 16);
-			temp.set(2, value >> 8);
-			temp.set(3, value);
-		} else {
-			temp.set(0, value);
-			temp.set(1, value >> 8);
-			temp.set(2, value >> 16);
-			temp.set(3, value >> 24);
-		}
+		value = BinaryStream.limit(value, 32, signed);
+		BinaryStream.writeValueIntoBytes(temp, 32, this.isBigEndian, value);
 		this.write(temp);
 	}
 
-	public function writeInt64(value:Int64):Void {
-		this.writeInt32(value.high);
-		this.writeInt32(value.low);
+	public function writeInt64(value:Int64, signed:Bool):Void {
+		this.writeInt32(value.high, signed);
+		this.writeInt32(value.low, signed);
 	}
 
 	public function writeFloat(value:Float):Void {
-		this.writeInt32(FPHelper.floatToI32(value));
+		this.writeInt32(FPHelper.floatToI32(value), true);
 	}
 
 	public function writeDouble(value:Float):Void {
-		var val: Int64 = FPHelper.doubleToI64(value);
-		this.writeInt32(val.high);
-		this.writeInt32(val.low);
+		var idouble:Int64 = FPHelper.doubleToI64(value);
+		this.writeInt32(idouble.high, true);
+		this.writeInt32(idouble.low, true);
 	}
 
 	public function writeVarInt(value:Int32):Void {
@@ -113,7 +167,7 @@ class BinaryStream {
 			var toWrite = value & 0x7f;
 			value >>>= 7;
 			if (value != 0x00) {
-				this.writeInt8(toWrite | 0x80 false);
+				this.writeInt8(toWrite | 0x80, false);
 			} else {
 				this.writeInt8(toWrite, false);
 				break;
@@ -136,57 +190,37 @@ class BinaryStream {
 	}
 
 	public function readInt8(signed:Bool):Int {
-		return this.read(1) & (signed == true ? 0x7f : 0xff);
-	}
-
-	public function readBool():Bool {
-		return this.read(false) == 1 ? true : false;
-	}
-
-	public function readInt16(signed:Bool):Int {
-		var value:Int = 0;
-		var v:Int = signed == true ? 0x7fff : 0xffff;
-		if (this.isBigEndian) {
-			value |= (this.read(1) & v) << 8;
-			value |= this.read(1) & v;
-		} else {
-			value |= this.read(1) & v;
-			value |= (this.read(1) & v) << 8;
-		}
+		var value:Int = this.read(1).get(0);
+		value = BinaryStream.limit(value, 8, signed);
 		return value;
 	}
 
-	public function readInt32():Int32 {
-		var value:Int32 = 0;
-		if (this.isBigEndian) {
-			value |= this.read(1) << 24;
-			value |= this.read(1) << 16;
-			value |= this.read(1) << 8;
-			value |= this.read(1);
-		} else {
-			value |= this.read(1);
-			value |= this.read(1) << 8;
-			value |= this.read(1) << 16;
-			value |= this.read(1) << 24;
-		}
+	public function readBool():Bool {
+		return this.readInt8(false) == 1 ? true : false;
+	}
+
+	public function readInt16(signed:Bool):Int {
+		var value:Int = BinaryStream.readValueFromBytes(this.read(2), 16, this.isBigEndian);
+		value = BinaryStream.limit(value, 16, signed);
+		return value;
+	}
+
+	public function readInt32(signed:Bool):Int32 {
+		var value:Int32 = BinaryStream.readValueFromBytes(this.read(4), 32, this.isBigEndian);
+		value = BinaryStream.limit(value, 32, signed);
 		return value;
 	}
 
 	public function readInt64():Int64 {
-		return Int64.make(this.readInt32(), this.readInt32());
+		return Int64.make(this.readInt32(false), this.readInt32(false));
 	}
 
 	public function readFloat():Float {
-		var value:Int = 0;
-		value |= this.read(1) << 24;
-		value |= this.read(1) << 16;
-		value |= this.read(1) << 8;
-		value |= this.read(1);
-		return FPHelper.i32ToFloat(value);
+		return FPHelper.i32ToFloat(this.readInt32(true));
 	}
 
 	public function readDouble():Float {
-		var value:Int64 = Int64.make(this.readInt32(), this.readInt32());
+		var value:Int64 = Int64.make(this.readInt32(false), this.readInt32(false));
 		return FPHelper.i64ToDouble(value.low, value.high);
 	}
 
@@ -216,6 +250,10 @@ class BinaryStream {
 	}
 
 	public function readZigZag64():Int64 {
-		return Int64.make(this.readZigZag32(), this.readZigZag33());
+		return Int64.make(this.readZigZag32(), this.readZigZag32());
+	}
+
+	public function readRemaining() {
+		return this.read(this.buffer.length - this.readingPos);
 	}
 }
